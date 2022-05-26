0897a79232d53a77724e9ece98f15b3c1e4d2c3f
Arno-Pm <70451630+Arno-Pm@users.noreply.github.com>
2022-05-26 15:31:46 +0300

    Update Book.js

--- src/Book.js
+++ src/Book.js
@@ -13,4 +13,36 @@ import { User } from './User.js';
  * @property {User[]} likedUsers
  * @property {User} publicationBy
  */
-export function Book(title, year, publicationBy, authors) {}
+export function Book(title, year, publicationBy, authors) {
+
+    this.title = title;
+    this.authors = authors;
+    this.year = year;
+    this.publicationBy = publicationBy;
+    this.likedUsers = [];
+    authors.forEach((author) => {
+        author.books.push(this);
+    });
+
+    publicationBy.myBooks.push(this);
+
+    Object.defineProperty(this, 'suggestedBooks', {
+        get() {
+            return this.authors.reduce((accum, { books }) => {
+                const allBooks = books.filter((book) => book !== this);
+                return [...new Set([...accum, ...allBooks])];
+            }, []).map(({title}) => title).join(', ');
+        }
+    });
+
+    Object.defineProperty(this, 'suggestedPublicators', {
+        get() {
+            return this.authors.reduce((accum, { books }) => {
+                const users = books.filter(({publicationBy}) => publicationBy !== this.publicationBy).map(({ publicationBy }) => publicationBy);
+
+                return [...new Set([...accum, ...users])]
+            }, []).map(({name}) => name).join(', ');
+        }
+    });
+}
+
